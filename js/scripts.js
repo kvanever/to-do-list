d50783a3d7b77b7a8665b314e449f2768f84c93d
Kevin VanEvery and Leah Nelson <leah@>
2016-06-13 10:42:47 -0700

    add logic to output list of to do items

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,35 @@ function Task(task, notes, date, completed) {
   this.completed = false;
 }
 
+Task.prototype.markCompleted = function(task) {
+  this.completed = true;
+}
 
 $(document).ready(function(){
 
-  $("#input").submit(function(event){
+  $("form#input").submit(function(event){
     event.preventDefault();
-    var input = ($("#blank").val());
 
-    var output = "";
+    var task = ($("input#task").val());
+    var notes = ($("input#notes").val());
+    var date = ($("input#date").val());
+    var completed = false;
 
-    $('#output').text(output);
+    var output = new Task(task, notes, date, completed);
 
+    $("div#output ul").append("<li><span class='task " + output.task + "'>" + output.task + "</span></li>");
+
+    $(".task").last().click(function() {
+    $("#full-detail").show();
+    $("#full-detail h2").text(output.task);
+    $(".notes").text(output.notes);
+    $(".date").text(output.date);
+    });
+
+    $("#full-detail h2").click(function () {
+      $("#full-detail").hide();
+      $("li." + output.task).remove();
+
+    })
   });
 });
