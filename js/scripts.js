bba0938b4f81569c0a558bb6ac07b05960c8a351
Kevin VanEvery and Leah Nelson <leah@>
2016-06-13 11:41:45 -0700

    remove completed parameter

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ function Task(task, notes, date, completed) {
   this.task = task;
   this.notes = notes;
   this.date = date;
-  this.completed = false;
 }
 
 
@@ -14,9 +13,8 @@ $(document).ready(function(){
     var task = ($("input#task").val());
     var notes = ($("input#notes").val());
     var date = ($("input#date").val());
-    var completed = false;
 
-    var output = new Task(task, notes, date, completed);
+    var output = new Task(task, notes, date);
 
     $("div#output ul").append("<li class='task'><h2>Task: " + output.task + "</h2><p>Notes: " + output.notes + "</p><p>Date: " + output.date + "</p></li>");
 
